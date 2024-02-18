4780f98801c68228ee553ceb72b71356190804e0
Simon Katan <simon@simonkatan.co.uk>
2024-02-18 18:42:02 +0000

    refactored into transforma

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import * as Tone from "tone";
 import { choose, deepChoose, randomArray } from "./utils";
 
-/////////////// sequence state //////////////////
+/////////////// global state //////////////////
 
-const notePool = ["A", "B", "C", "D", "E", "F", "G", "A", "F#", "C#"];
 let seq;
-let currIdx;
-let lastAdditionIdx;
-let numReps = 3;
+let playbackBeat;
+let currentTransformState;
 
 //////////////// tonejs globals //////////////////
 
@@ -23,10 +21,13 @@ document.querySelector("#start-audio").addEventListener("click", () => {
 });
 
 document.querySelector("#start")?.addEventListener("click", async () => {
-  seq = new Array(10);
-  seq[0] = { note: getNextNote(), pan: getNextPan() };
-  currIdx = 0;
-  lastAdditionIdx = 0;
+  playbackBeat = 0;
+  currentTransformState = null;
+  seq = new Array(20);
+  seq[0] = {
+    note: "A3",
+    pan: 0
+  };
   Tone.Transport.start();
 });
 
@@ -38,12 +39,12 @@ document.querySelector("#stop")?.addEventListener("click", async () => {
 
 async function setup() {
   await Tone.start();
-  Tone.Transport.bpm = 90;
+  Tone.Transport.bpm = 100;
   masterChannel = new Tone.Channel({ channelCount: 2 });
   masterChannel.toDestination();
   setupMeter();
   // create the sequencer loop
-  new Tone.Loop(loop, "8n").start(0);
+  new Tone.Loop(loop, "16n").start(0);
 }
 
 function setupMeter() {
@@ -60,7 +61,7 @@ function setupMeter() {
     const amps = Math.pow(Tone.dbToGain(dbs), 0.5) * 200;
     context.fillStyle = `rgb(255,0,0)`;
     context.fillRect(x, canvas.height - amps, 100, amps);
-    context.fillStyle = `rgb(0,0)`;
+    context.fillStyle = `rgb(0,0,0)`;
     context.fillText(amps, x + 50, 50);
   };
 
@@ -71,8 +72,18 @@ function setupMeter() {
     context.fillRect(0, 0, canvas.width, canvas.height);
     drawVolume(dbs[0], 75);
     drawVolume(dbs[1], 225);
+    context.fillStyle = `rgb(0,0,0)`;
+    context.fillText(`playbackBeat: ${playbackBeat}`, 0, 50);
 
-    window.requestAnimationFrame(updateMeter);
+    if (currentTransformState) {
+      context.fillText(
+        `${currentTransformState.cyclesUntilNextAction}`,
+        0,
+        100
+      );
+    }
+
+    context, window.requestAnimationFrame(updateMeter);
   };
 
   updateMeter();
@@ -81,28 +92,11 @@ function setupMeter() {
 ////////////////////////////////// Loop functions ///////////////////////////////
 
 function loop(time) {
-  if (seq[currIdx]) {
-    playCurrentNote(seq[currIdx], time);
+  if (seq[playbackBeat]) {
+    playCurrentNote(seq[playbackBeat], time);
   }
-
-  currIdx = (currIdx + 1) % seq.length;
-  if (currIdx === 0) {
-    console.log(numReps);
-    if (numReps === 0) {
-      const additionIdx = (lastAdditionIdx + 7) % seq.length;
-      const note = getNextNote(seq[lastAdditionIdx]?.note);
-      seq[additionIdx] = note
-        ? {
-            note,
-            pan: getNextPan()
-          }
-        : null;
-      lastAdditionIdx = additionIdx;
-      numReps = choose([1, 2, 3]);
-    } else {
-      numReps -= 1;
-    }
-  }
+  currentTransformState = applyTransforms(currentTransformState);
+  playbackBeat = (playbackBeat + 1) % seq.length;
 }
 
 function playCurrentNote(note, time) {
@@ -127,26 +121,132 @@ function playCurrentNote(note, time) {
   synth.triggerAttackRelease(note.note, "16n", time);
 }
 
-function getNextNote(prevNote) {
+////////////////////////////////
+
+function applyTransforms(_currentTransformState) {
+  let _transformState = _currentTransformState
+    ? { ..._currentTransformState }
+    : initFillSequence();
+
+  if (_transformState.isComplete) {
+    console.log(_transformState);
+    _transformState = chooseTransform(_transformState);
+  } else {
+    switch (_transformState.transform) {
+      case "fill-sequence":
+        _transformState = fillSequence(_transformState);
+        break;
+      case "reduce-sequence":
+        _transformState = reduceSequence(_transformState);
+        break;
+      default:
+        throw new Error("transform not found");
+    }
+  }
+
+  return _transformState;
+}
+
+function chooseTransform(_transformState) {
+  if (_transformState.transform === "fill-sequence") {
+    return initReduceSequence();
+  } else {
+    return initFillSequence();
+  }
+}
+
+//////////////////////////////// Transforms /////////////////////////////
+
+function initFillSequence() {
+  return {
+    transform: "fill-sequence",
+    notePool: ["A", "B", "C", "D", "E", "F", "G", "A", "F#", "C#"],
+    lastAdditionOnBeat: 0,
+    cyclesUntilNextAction: 3,
+    isComplete: false
+  };
+}
+
+function fillSequence(_currentTransformState) {
+  const _transformState = { ..._currentTransformState };
+  _transformState.isComplete = _transformState.notePool.length === 0;
+
+  if (playbackBeat === 0) {
+    if (_transformState.cyclesUntilNextAction === 0) {
+      const beat = (_transformState.lastAdditionOnBeat + 14) % seq.length;
+      const note = getNextNote(_transformState);
+
+      if (note) {
+        seq[beat] = {
+          note,
+          pan: getNextPan()
+        };
+      }
+      _transformState.lastAdditionOnBeat = beat;
+      _transformState.cyclesUntilNextAction = choose([1]);
+    } else {
+      _transformState.cyclesUntilNextAction -= 1;
+    }
+  }
+
+  return _transformState;
+}
+
+function getNextNote(_currentTransformState) {
+  const { notePool, lastAdditionOnBeat = 0 } = _currentTransformState;
+  const prevNote = seq[lastAdditionOnBeat].note;
+
   if (notePool.length === 0) return undefined;
   const chroma = deepChoose(notePool);
   if (!prevNote) return chroma + 3;
+
   const prevOctave = Number(prevNote.substring(prevNote.length - 1));
 
-  console.log({ prevOctave });
   if (
     Tone.Frequency(chroma + prevOctave).toMidi() <
     Tone.Frequency(prevNote).toMidi()
   ) {
-    console.log(chroma + Math.min(prevOctave + 1, 6));
     return chroma + Math.min(prevOctave + 1, 6);
   } else {
-    console.log(chroma + prevOctave);
     return chroma + prevOctave;
   }
 }
 
-function getNextPan(centroidBias = 1) {
+function getNextPan(_currentTransformState, centroidBias = 1) {
   const pan = -1 + Math.pow(Math.random(), centroidBias) * 2;
   return pan;
+}
+
+function initReduceSequence() {
+  return {
+    transform: "reduce-sequence",
+    lastSubtractionOnBeat: 0,
+    cyclesUntilNextAction: 3,
+    isComplete: false
+  };
+}
+
+function reduceSequence(_currentTransformState) {
+  let _transformState = { ..._currentTransformState };
+
+  const notesRemaining = seq.reduce((prev, curr) => prev + (curr ? 1 : 0), 0);
+
+  console.log(notesRemaining);
+
+  if (notesRemaining === 3) {
+    _transformState.isComplete = true;
+  }
+
+  if (playbackBeat === 0) {
+    if (_transformState.cyclesUntilNextAction === 0) {
+      const beat = (_transformState.lastSubtractionOnBeat + 14) % seq.length;
+      seq[beat] = undefined;
+      _transformState.lastSubtractionOnBeat = beat;
+      _transformState.cyclesUntilNextAction = choose([1, 2, 3]);
+      console.log(seq);
+    } else {
+      _transformState.cyclesUntilNextAction -= 1;
+    }
+  }
+  return _transformState;
 }
