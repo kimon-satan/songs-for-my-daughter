339a7ca1b01180de41fcce96ae92c064f9e53b1b
Simon Katan <simon@simonkatan.co.uk>
2024-02-18 11:57:58 +0000

    metering

--- index.js
+++ index.js
@@ -25,44 +25,59 @@ console.log(seq);
 let currIdx = 0;
 let lastAdditionIdx = 0;
 let numReps = 4;
-let isAudioContextStarted;
 
 document.querySelector("#start-audio").addEventListener("click", async () => {
   await Tone.start();
-  alert("Tone ready");
+  setup();
   const matches = document.querySelectorAll(".control-button");
   matches.forEach((m) => (m.disabled = false));
   document.querySelector("#start-audio").disabled = true;
 });
 
 document.querySelector("#start")?.addEventListener("click", async () => {
-  if (!isAudioContextStarted) {
-    await Tone.start();
-    isAudioContextStarted = true;
-    const loop = new Tone.Loop((time) => {
-      if (seq[currIdx]) {
-        polySynth.triggerAttackRelease(seq[currIdx], "16n", time);
-      }
-      currIdx = (currIdx + 1) % 10;
-      if (currIdx === 0) {
-        if (numReps === 0) {
-          const additionIdx = (lastAdditionIdx + 7) % 10;
-          seq[additionIdx] = getNextNote(seq[lastAdditionIdx]);
-          lastAdditionIdx = additionIdx;
-          numReps = choose([1, 2, 3]);
-        } else {
-          numReps -= 1;
-        }
-      }
-    }, "8n").start(0);
-    Tone.Transport.bpm = 90;
-  }
   Tone.Transport.start();
 });
 
 document.querySelector("#stop")?.addEventListener("click", async () => {
   Tone.Transport.stop();
 });
+
+function setup() {
+  const loop = new Tone.Loop((time) => {
+    if (seq[currIdx]) {
+      polySynth.triggerAttackRelease(seq[currIdx], "16n", time, Math.random());
+    }
+    currIdx = (currIdx + 1) % 10;
+    if (currIdx === 0) {
+      if (numReps === 0) {
+        const additionIdx = (lastAdditionIdx + 7) % 10;
+        seq[additionIdx] = getNextNote(seq[lastAdditionIdx]);
+        lastAdditionIdx = additionIdx;
+        numReps = choose([1, 2, 3]);
+      } else {
+        numReps -= 1;
+      }
+    }
+  }, "8n").start(0);
+  Tone.Transport.bpm = 90;
+
+  const meter = new Tone.Meter();
+  polySynth.connect(meter);
+  const canvas = document.querySelector("#meter");
+  const context = canvas.getContext("2d");
+
+  const drawVolume = () => {
+    const dbs = meter.getValue();
+    const amps = Math.pow(Tone.dbToGain(dbs), 0.5) * 100;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.arc(100, 100, amps, 0, 2 * Math.PI);
+    context.stroke();
+    window.requestAnimationFrame(drawVolume);
+  };
+
+  drawVolume();
+}
 
 function getNextNote(prevNote) {
   if (notePool.length === 0) return undefined;
