cfec191b181f3e17365da3c5a93ab8627ea72407
Simon Katan <simon@simonkatan.co.uk>
2024-03-03 14:27:51 +0000

    tests

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 Planning:
 https://www.figma.com/file/WxgIthxGLw0mnNocuy4u3T/Songs-For-My-Daughter---transitions?type=whiteboard&node-id=0-1&t=dAFDVxvXhbn1Mib1-0
 
-[] Args for Silence Beats modulo
-[] Variable methods of note choice
 [] implement activateMultipleBeats & silenceMultipleBeats
+
+[] Variable methods of note choice
 
 [] then look at how to link various segments together
--- src/transforms.test.js
+++ src/transforms.test.js
@@ -1,6 +1,12 @@
 import { describe, expect, it } from "vitest";
-import { activateBeatsModulo, initActivateBeatsModulo } from "./transforms";
+import {
+  activateBeatsModulo,
+  initActivateBeatsModulo,
+  silenceBeatsModulo,
+  initSilenceBeatsModulo
+} from "./transforms";
 import { getActiveBeats } from "./utils";
+import { sequenceFixtures } from "./sequence.fixtures";
 
 describe("transforms", () => {
   describe("activateBeatsModulo", () => {
@@ -68,6 +74,65 @@ describe("transforms", () => {
         expect(getActiveBeats(seq)).toHaveLength(i + 1);
         expect(transformState.isComplete).toEqual(
           i === transformState.maxReps - 1
+        );
+      }
+    });
+  });
+
+  describe("silenceBeatsModulo", () => {
+    it("removes a note from a sequence", () => {
+      const seq = sequenceFixtures.allBeatsChromatic();
+      const transformState = initSilenceBeatsModulo({ _seq: seq });
+
+      const { _seq, _transformState } = silenceBeatsModulo({
+        _seq: seq,
+        _transformState: transformState
+      });
+
+      expect(getActiveBeats(_seq)).toHaveLength(getActiveBeats(seq).length - 1);
+    });
+    it("it completes when the sequence reaches minBeats", () => {
+      let seq = sequenceFixtures.allBeatsChromatic();
+      let transformState = initSilenceBeatsModulo({ _seq: seq });
+      transformState.minBeats = 5;
+      transformState.maxReps = 100;
+      transformState.modulo = 1;
+
+      for (
+        let i = getActiveBeats(seq).length;
+        i > transformState.minBeats;
+        i--
+      ) {
+        const { _seq, _transformState } = silenceBeatsModulo({
+          _seq: seq,
+          _transformState: transformState
+        });
+        transformState = _transformState;
+        seq = _seq;
+        expect(getActiveBeats(seq)).toHaveLength(i - 1);
+        expect(transformState.isComplete).toEqual(
+          i - 1 === transformState.minBeats
+        );
+      }
+    });
+
+    it("it doesn't exceed maxReps", () => {
+      let seq = sequenceFixtures.allBeatsChromatic();
+      let transformState = initSilenceBeatsModulo({ _seq: seq });
+      transformState.minBeats = 1;
+      transformState.maxReps = 10;
+      transformState.modulo = 1;
+
+      for (let i = 0; i < transformState.maxReps; i++) {
+        const { _seq, _transformState } = silenceBeatsModulo({
+          _seq: seq,
+          _transformState: transformState
+        });
+        transformState = _transformState;
+        seq = _seq;
+
+        expect(transformState.isComplete).toEqual(
+          i + 1 === transformState.maxReps
         );
       }
     });
